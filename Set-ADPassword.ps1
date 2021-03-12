a27fa62bad885c50397fe467570354449a2255be
Matthew <61301618+Matthew-git-hub@users.noreply.github.com>
2021-03-12 15:53:10 -0500

    Create Set-ADPassword.ps1

---
+++ Set-ADPassword.ps1
@@ -0,0 +1,20 @@
+function Set-ADPassword {
+    [cmdletBinding()]
+
+      param (
+  
+      #Username of the user to be reset 
+      [Parameter(Mandatory)]
+      [string]$username,
+
+ 
+
+      #New Password
+      [Parameter(Mandatory)]
+      [SecureString]$password
+      )
+      
+      Set-AdAccountPassword -Identity $username -NewPassword $securePassword -Reset 
+      Set-AdUSer -identity $username -ChangePasswordAtLogon $true
+  
+  }
