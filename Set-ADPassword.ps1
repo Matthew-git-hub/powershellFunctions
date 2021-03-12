9bfe21e6c65be8ef8d33f9ae86967ab519c80e96
Matthew <61301618+Matthew-git-hub@users.noreply.github.com>
2021-03-12 16:09:39 -0500

    Update Set-ADPassword.ps1
    Added Synopsis and made more human readable

--- Set-ADPassword.ps1
+++ Set-ADPassword.ps1
@@ -1,18 +1,33 @@
+<#
+.SYNOPSIS
+This cmdlet is for easily reseting an active directory password
+
+.PARAMETER Username
+Enter a string containing the domain or workgroup of the user and the username or in some cases just the username.    
+               
+.PARAMETER Passwd
+Enter the string value of the users new password    
+#>
+
 function Set-ADPassword {
-    [cmdletBinding()]
+  [cmdletBinding()]
 
-      param (
-  
-      #Username of the user to be reset 
-      [Parameter(Mandatory)]
-      [string]$username,
+    param (
 
-      #New Password
-      [Parameter(Mandatory)]
-      [SecureString]$password
-      )
-      
-      Set-AdAccountPassword -Identity $username -NewPassword $securePassword -Reset 
-      Set-AdUSer -identity $username -ChangePasswordAtLogon $true
-  
-  }
+    #Username of the user to be reset 
+    [Parameter(
+      Mandatory=$true,
+      HelpMessage="Enter The Username: ")]
+    [string]$username,
+
+    #New Password
+    [Parameter(
+      Mandatory=$true,
+      HelpMessage="Enter The New Password")]
+    [SecureString]$password
+    )
+    
+    Set-AdAccountPassword -Identity $username -NewPassword $securePassword -Reset 
+    Set-AdUSer -identity $username -ChangePasswordAtLogon $true
+
+}
